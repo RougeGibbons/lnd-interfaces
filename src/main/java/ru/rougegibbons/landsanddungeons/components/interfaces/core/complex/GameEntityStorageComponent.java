d91ac964b0f3959d876bf385e672f6eb4e34c183
Kirill Cherkasov <kvcherk@live.ru>
2018-11-05 01:46:15 +0300

    checkstyle fix

--- src/main/java/ru/rougegibbons/landsanddungeons/components/interfaces/core/complex/GameEntityStorageComponent.java
+++ src/main/java/ru/rougegibbons/landsanddungeons/components/interfaces/core/complex/GameEntityStorageComponent.java
@@ -6,8 +6,7 @@ import ru.rougegibbons.landsanddungeons.components.interfaces.core.mixins.Packab
 import ru.rougegibbons.landsanddungeons.entities.interfaces.core.GameEntity;
 
 /**
- * generified interface for components that may store some ingame entities,
- * e.g. backpack/bank slots, spellbook, talent trees, etc.
+ * generified interface for components that may store some ingame entities, e.g. backpack/bank slots or spellbooks.
  *
  * @param <T> - any class implementing  {@link GameEntity} interface or its extensions.
  * @see GameEntity
